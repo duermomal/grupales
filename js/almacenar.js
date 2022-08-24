6180f43a537fdf50d1de61a244e20713dbff023d
duermomal <110434883+duermomal@users.noreply.github.com>
2022-08-24 19:59:15 -0300

    avance gs

--- js/almacenar.js
+++ js/almacenar.js
@@ -3,11 +3,18 @@ const agregar = document.getElementById("agregar");
 const limpiar = document.getElementById("limpiar");
 const nuevoItem = document.getElementById("item");
 const contenedor = document.getElementById("contenedor");
+let items = [];
 
 
-agregar.addEventListener("click", (evt) => {
+
+agregar.addEventListener("click", (evt) => { 
     // Si tenemos texto ingresado en el input, lo guardamos en el localStorage
-    for (let i = 0);
-    if (nuevoItem.value) localStorage.setItem("text", nuevoItem.value);  
+    if (nuevoItem.value) {
+       if(localStorage.getItem('texto')){
+            items = JSON.parse(localStorage.getItem('texto'));
+       }
+    items.push(nuevoItem.value);
+    
+        
+    } 
 });
- 
